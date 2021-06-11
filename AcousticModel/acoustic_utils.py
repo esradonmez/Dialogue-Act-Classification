fe1c6317cece0339f955530a8368842220f800ea
Esra <56908455+esradonmez@users.noreply.github.com>
2021-06-11 17:54:38 +0200

    speechCNN + utils.pad_features

--- AcousticModel/acoustic_utils.py
+++ AcousticModel/acoustic_utils.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 # load mfcc features
 file_path = "/mount/studenten/dialog-system/2021/teams/mauldasch/features/audio_feat.json"  
@@ -14,3 +15,21 @@ def get_max_length(data):
         if len(v) > max:
             max = len(v)
     return max
+
+# pad the input
+def pad_features(data, max_len):
+  # get the values
+  x = []
+  for k,v in data.items():
+    print(v)
+    x.append(v)
+  
+  # pad to the max_lenght
+  zeros = np.zeros(13)
+  padded = []
+  for i in x:
+    temp = i
+    temp.append(((max_len - len(i)) * zeros))
+    padded.append(temp)
+  
+  return padded
