fe7ce10320d012851b17ca351e3ce4b496247543
chris <chris.schaller@posteo.de>
2021-06-14 18:46:33 +0200

    feat: use Adam Optimizer

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
+from sklearn.metrics import accuracy_score
 from torch.utils.data import DataLoader
 
 from acoustic_model.speech_cnn import SpeechCnn
@@ -8,38 +10,52 @@ from acoustic_model.dac_dataset import DacDataset
 DATA_PATH = "./data"
 
 if __name__ == '__main__':
+    learning_rate = 0.001
+    batch_size = 8
+    epochs = 5
+
     model = SpeechCnn()
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
     dataset = DacDataset(
         f"{DATA_PATH}/train.txt",
         f"{DATA_PATH}/audio_features"
     )
-    dataloader = DataLoader(dataset, batch_size=8)
+    dataloader = DataLoader(dataset, batch_size=batch_size)
 
-    for epoch in range(2):
+    for epoch in range(epochs):
+        gold_labels = []
+        pred_labels = []
 
         running_loss = 0.0
         for i, data in enumerate(dataloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
+
             labels, inputs = data
+            gold_labels.extend(labels)
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
-            # forward + backward + optimize
+            # forward
             outputs = model(inputs.float())
-            # print(f"{outputs=}")
+
+            pred_labels.extend(torch.argmax(torch.softmax(outputs, 1), 1))
+
+            # backward + optimize
             loss = criterion(outputs, labels)
-            # print(f"{loss=}")
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(
+                    f"[{epoch + 1}\ti+1]\tloss: {running_loss / 2000}\t"
+                    f"acc: {accuracy_score(gold_labels, pred_labels)}")
                 running_loss = 0.0
+
+        print(
+            f"Finished epoch {epoch + 1}\t"
+            f"acc: {accuracy_score(gold_labels, pred_labels)}")
