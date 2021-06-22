f94427d6bd5e9210dd036e57b2a0c175905b5677
chris <chris.schaller@posteo.de>
2021-06-22 20:03:13 +0200

    feat: add combined model class

---
+++ combined_model/combined_model.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+
+
+class CombinedModel(nn.Module):
+    def __init__(self,
+                 acoustic_model: nn.Module,
+                 lexical_model: nn.Module,
+                 lexical_tokenizer
+                 ):
+        super().__init__()
+
+        self.acoustic_model = acoustic_model
+        self.lexical_model = lexical_model
+        self.lexical_tokenizer = lexical_tokenizer
+
+        self.fc = nn.Linear(
+            self.acoustic_model.output_dimension +
+            self.lexical_model.output_dimension, 4)
+
+    def forward(self, combined_input):
+        lexical_input, acoustic_input = combined_input
+
+        acoustic_x = self.acoustic_model(acoustic_input.float())
+        lexical_x = self.lexical_model(**self.lexical_tokenizer(
+            lexical_input,  # Sentence to encode.
+            add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+            max_length=128,  # Pad & truncate all sentences.
+            pad_to_max_length=True,
+            return_attention_mask=True,  # Construct attn. masks.
+            return_tensors='pt',  # Return pytorch tensors.
+        ))
+
+        # print("acoustic_x", acoustic_x.size())
+        # print("lexical_x", lexical_x.size())
+
+        # concat
+        concat_x = torch.cat([acoustic_x, lexical_x.view(-1, 128*100)], dim=1)
+
+        return self.fc(concat_x)
