ad904476c341590f64c50c19c95185e83883077a
chris <chris.schaller@posteo.de>
2021-07-02 12:02:40 +0200

    feat: add variable kernel sizes for conv and pool layer

--- acoustic_model/speech_cnn.py
+++ acoustic_model/speech_cnn.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import torch
 import torch.nn.functional as F
 from torch import nn
@@ -12,27 +14,38 @@ def pool_output_dim(input_dim, kernel, stride, padding=0):
 
 
 class SpeechCnn(nn.Module):
-    def __init__(self):
+    def __init__(self,
+                 conv_kernel: Tuple[int, int] = (5, 5),
+                 pool_kernel: Tuple[int, int] = (5, 5),
+                 pool_stride: int = 3,
+                 output_dimension: int = 50
+                 ):
         super().__init__()
 
-        # to reduce the dimensions, [50,100] is fine
-        self.output_dimension = 50
+        # to reduce the dimensions, [50, 100] is fine
+        self.output_dimension = output_dimension
+
+        self.conv_kernel_height, self.conv_kernel_width = conv_kernel
+        self.pool_kernel_height, self.pool_kernel_width = pool_kernel
+        self.pool_stride = pool_stride
 
         self.output_pool_width = pool_output_dim(
-            conv_output_dim(13, 5, 1, 0), 5, 3, 0)
+            conv_output_dim(13, self.conv_kernel_width, 1, 0),
+            self.pool_kernel_width, self.pool_stride, 0)
         self.output_pool_height = pool_output_dim(
-            conv_output_dim(3361, 5, 1, 0), 5, 3, 0)
+            conv_output_dim(3361, self.conv_kernel_height, 1, 0),
+            self.pool_kernel_height, self.pool_stride, 0)
 
         self.conv = nn.Conv2d(
             in_channels=1,
             out_channels=6,
-            kernel_size=(5, 5)
-        )  # i'm not sure about these
+            kernel_size=(self.conv_kernel_height, self.conv_kernel_width)
+        )
 
         self.pool = nn.MaxPool2d(
-            kernel_size=(5, 5),
-            stride=3
-        )  # ask daniel
+            kernel_size=(self.pool_kernel_height, self.pool_kernel_width),
+            stride=self.pool_stride
+        )
 
         self.fc = nn.Linear(
             self.output_pool_width * self.output_pool_height * 6,
