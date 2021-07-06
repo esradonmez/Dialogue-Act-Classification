c3015b8f565689a188af2e1723d08d2071e623ec
chris <chris.schaller@posteo.de>
2021-07-06 13:07:58 +0200

    refactor: move mfcc_features.py to dataset package

--- dataset/mfcc_features.py
+++ dataset/mfcc_features.py
@@ -7,8 +7,6 @@ import scipy.io.wavfile as wav
 from python_speech_features import mfcc
 from tqdm import tqdm
 
-DATA_PATH = "../data"
-
 
 def prepare_folder(folder_path: Path):
     if not folder_path.exists():
@@ -29,18 +27,9 @@ def generate_mfcc_features(input_dir: Path, output_dir: Path):
         if mfcc_feat.shape[0] > max_len:
             max_len = mfcc_feat.shape[0]
 
-        with open(Path(output_dir, f"{audio_file.stem}.npy"), "wb") as file_pointer:
-            np.save(file_pointer, mfcc_feat)
+        np.save(f"{audio_file.stem}.npy", mfcc_feat)
 
     with open(Path(output_dir, "meta.json"), "w") as file_pointer:
         json.dump({
             "max_len": max_len
         }, file_pointer)
-
-
-if __name__ == '__main__':
-    prepare_folder(Path(DATA_PATH, "audio_features"))
-
-    generate_mfcc_features(
-        Path(DATA_PATH, "audios"),
-        Path(DATA_PATH, "audio_features"))
